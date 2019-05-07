5a31b7cf971a1360927401b63218abc2967eaa5e
laszlosimity <lsimity@gmail.com>
2019-05-07 10:17:09 +0200

    Upgrade to 5.x

--- tests/smoketests.js
+++ tests/smoketests.js
@@ -3,14 +3,18 @@ const HomePage = require('../pages/homepage');
 const homePage = new HomePage();
 const InventoryPage = require('../pages/inventorypage');
 const invPage = new InventoryPage();
+const { addArgument } = require ('@wdio/allure-reporter').default;
+
 
 describe('Sauce Swag smoketests ', function() {
 
     it("at homepage", function() {
+        //addArgument('Device Name', browser.capabilities.testobject_device_name);
         expect(homePage.title_image.isExisting()).to.be.true;
     });
 
     it("logs in", function() {
+        //addArgument('Device Name', browser.capabilities.testobject_device_name);
     	homePage.setUsername("standard_user");
     	homePage.setPassword("secret_sauce");
     	homePage.login();
@@ -18,7 +22,8 @@ describe('Sauce Swag smoketests ', function() {
     });
 
     it ("logs out", function() {
-    	invPage.logout();
+        //addArgument('Device Name', browser.capabilities.testobject_device_name);
+        invPage.logout();
     	expect(homePage.getUsername().isExisting()).to.be.true;
     });
 
